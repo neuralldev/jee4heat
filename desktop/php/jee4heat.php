69dab2c802d352f215d79ec23d0892bb1984e4a8
neuralldev <thierry.gluzman@neurall.fr>
2023-10-29 17:37:57 +0100

    ,k

--- desktop/php/jee4heat.php
+++ desktop/php/jee4heat.php
@@ -129,8 +129,8 @@ $eqLogics = eqLogic::byType($plugin->getId());
                                 <div class="form-group">
                                     <label class="col-md-4 control-label">{{Modèle}}</label>
                                     <div class="col-md-6">
-                                        <select id="idmodele" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="modele"
-                                                onchange="if(this.selectedIndex == 2) document.getElementById('idmodele').style.display = 'block'; else document.getElementById('idmodele').style.display = 'none';">
+                                    <select id="typeEq" class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="type">
+                                        <select id="modeleEq" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="modele">
                                             <option value="generic">{{Générique}}</option>
                                             <option value="godin_artemis">{{Godin Artemis}}</option>
                                         </select>
