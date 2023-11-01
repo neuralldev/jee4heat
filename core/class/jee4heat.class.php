dce24d8b7ea9e879dbe8d87065aed459bce1bd28
neuralldev <thierry.gluzman@neurall.fr>
2023-11-01 12:01:25 +0100

    nb

--- core/class/jee4heat.class.php
+++ core/class/jee4heat.class.php
@@ -430,6 +430,7 @@ if you need to set an attribute for a register, change json depending on stove r
       log::add(__CLASS__, 'debug', "setpoint : new set point set to ".$v);
       if ($v > 0) {
         $register = substr($setpoint,-5);
+        log::add(__CLASS__, 'debug', "setpoint : trim logical ID".$setpoint.' to '.$register);
         $r=$this->setStoveValue($ip, $$register, $v);
         log::add(__CLASS__, 'debug', "setpoint : stove return ".$r);
 //        $cmd->event($v*100);
