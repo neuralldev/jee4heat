7662532dc2338910ee2c973d8a8138b0a3315b55
neuralldev <thierry.gluzman@neurall.fr>
2023-11-07 13:29:47 +0100

    x

--- core/class/jee4heat.class.php
+++ core/class/jee4heat.class.php
@@ -567,7 +567,7 @@ class jee4heat extends eqLogic
       }
     }
     $Equipement->AddCommand(__('Etat', __FILE__), 'jee4heat_stovestate', "info", "binary", 'heat', '', 'THERMOSTAT_STATE', 1, 'default', 'default', 'default', 'default', $order, '0', true, 'default', null, 2, null, null, null, 0);
-    $Equipement->AddCommand(__('Bloqué', __FILE__), 'jee4heat_stoveblocked', "info", "binary", 'alert', '', '', 1, 'default', 'default', 'default', 'default', $order, '0', true, 'default', null, 2, null, null, null, 1);
+    $Equipement->AddCommand(__('Bloqué', __FILE__), 'jee4heat_stoveblocked', "info", "binary", 'jee4heat::mylock', '', '', 1, 'default', 'default', 'default', 'default', $order, '0', true, 'default', null, 2, null, null, null, 1);
     $Equipement->AddCommand(__('Message', __FILE__), 'jee4heat_stovemessage', "info", "string", 'line', '', '', 1, 'default', 'default', 'default', 'default', $order, '0', true, 'default', null, 2, null, null, null, 0);
     $Equipement->setConfiguration('jee4heat_stovestate', STATE_REGISTER);
     log::add(__CLASS__, 'debug', 'check refresh in postsave');
@@ -664,6 +664,13 @@ class jee4heat extends eqLogic
           array('operation' => '#value# >= 4 && #value# <= 5','state_light' => '#value# Moyenne','state_dark' => '#value# Moyenne'),
           array('operation' => '#value# == 6','state_light' => '#value# Haute','state_dark' => '#value# Haute'),
           array('operation' => '#value# == 7','state_light' => 'Auto', 'state_dark' => 'Auto')
+        )
+      );
+      $return['info']['string']['mylocked'] = array(
+        'template' => 'tmplmultistate',
+        'test' => array(
+          array('operation' => '#value# == 0','state_light' => 'Non','state_dark' => 'Non'),
+          array('operation' => '#value# == 1','state_light' => 'Oui', 'state_dark' => 'Oui')
         )
       );
     return $return;
