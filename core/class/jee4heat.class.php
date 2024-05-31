59fafac59943fa319d7e6e1acbb1ea3650f78fc8
neuralldev <thierry.gluzman@neurall.fr>
2024-05-31 07:53:09 +0200

    syntax error

--- core/class/jee4heat.class.php
+++ core/class/jee4heat.class.php
@@ -296,7 +296,7 @@ class jee4heat extends eqLogic
               else
                 log::add(__CLASS__, 'debug', 'cron socket has returned which is not unpackable =' . $stove_return);
           }
-        } else
+        } 
       }
       log::add(__CLASS__, 'debug', 'equipment is disabled, cron skiped');
     }
